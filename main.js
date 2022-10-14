fa217eb1ff486b2bff3d340e2d0353a8bade351c
umamcag <43603523+umamcag@users.noreply.github.com>
2022-10-14 19:44:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function menu1(){
+document.getElementById("a1").style.backgroundColor ="yellow";
+document.getElementById("a2").style.backgroundColor ="paleturquoise";
+document.getElementById("a3").style.backgroundColor ="paleturquoise";
+document.getElementById("a4").style.backgroundColor ="paleturquoise";
+document.getElementById("a5").style.backgroundColor ="paleturquoise";   
+}
+
+function menu2(){
+    document.getElementById("a1").style.backgroundColor ="paleturquoise";
+    document.getElementById("a2").style.backgroundColor ="yellow";
+    document.getElementById("a3").style.backgroundColor ="paleturquoise";
+    document.getElementById("a4").style.backgroundColor ="paleturquoise";
+    document.getElementById("a5").style.backgroundColor ="paleturquoise";   
+    }
+
+    function menu3(){
+        document.getElementById("a1").style.backgroundColor ="paleturquoise";
+        document.getElementById("a2").style.backgroundColor ="paleturquoise";
+        document.getElementById("a3").style.backgroundColor ="yellow";
+        document.getElementById("a4").style.backgroundColor ="paleturquoise";
+        document.getElementById("a5").style.backgroundColor ="paleturquoise";   
+        }
+
+        function menu4(){
+            document.getElementById("a1").style.backgroundColor ="paleturquoise";
+            document.getElementById("a2").style.backgroundColor ="paleturquoise";
+            document.getElementById("a3").style.backgroundColor ="paleturquoise";
+            document.getElementById("a4").style.backgroundColor ="yellow";
+            document.getElementById("a5").style.backgroundColor ="paleturquoise";   
+            }
+
+            function menu5(){
+                document.getElementById("a1").style.backgroundColor ="paleturquoise";
+                document.getElementById("a2").style.backgroundColor ="paleturquoise";
+                document.getElementById("a3").style.backgroundColor ="paleturquoise";
+                document.getElementById("a4").style.backgroundColor ="paleturquoise";
+                document.getElementById("a5").style.backgroundColor ="yellow";   
+                }
